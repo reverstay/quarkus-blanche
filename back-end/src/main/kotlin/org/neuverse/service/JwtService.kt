31de7075c407c503103be60659e91577ca929f8e
Ramon Mariano <reversmon@gmail.com>
2025-11-17 12:30:05 -0300

    auth Jwt funcionando

--- back-end/src/main/kotlin/org/neuverse/service/JwtService.kt
+++ back-end/src/main/kotlin/org/neuverse/service/JwtService.kt
@@ -1,23 +1,37 @@
 package org.neuverse.service
 
 import jakarta.enterprise.context.ApplicationScoped
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.neuverse.entity.Usuario
 import io.smallrye.jwt.build.Jwt
+import java.nio.charset.StandardCharsets
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import javax.crypto.spec.SecretKeySpec
 
 @ApplicationScoped
-class JwtService {
+class JwtService(
+
+    @ConfigProperty(name = "jwt.secret")
+    private val jwtSecret: String
+) {
 
     fun generateToken(usuario: Usuario): String {
         val expiry = Instant.now().plus(1, ChronoUnit.HOURS)
 
-        return Jwt.issuer("neuverse-api")
+        // Chave HMAC para HS256
+        val signingKey = SecretKeySpec(
+            jwtSecret.toByteArray(StandardCharsets.UTF_8),
+            "HmacSHA256"
+        )
+
+        return Jwt
+            .issuer("neuverse-api")                     // bate com mp.jwt.verify.issuer
             .subject(usuario.id.toString())
             .upn(usuario.email)
-            .groups(setOf(usuario.cargo)) // grupos = cargos
+            .groups(setOf(usuario.cargo))               // depois pode trocar pra roles reais
             .claim("nome", usuario.nome)
             .expiresAt(expiry)
-            .sign()
+            .sign(signingKey)                           // assina com HS256 + segredo
     }
 }
--- back-end/src/main/kotlin/org/neuverse/service/PasswordService.kt
+++ back-end/src/main/kotlin/org/neuverse/service/PasswordService.kt
@@ -1,14 +1,19 @@
 package org.neuverse.service
 
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.mindrot.jbcrypt.BCrypt
 
 @ApplicationScoped
-class PasswordService {
+class PasswordService @Inject constructor(
+    @ConfigProperty(name = "auth.pepper")
+    private val pepper: String
+) {
 
     fun hash(rawPassword: String): String =
-        BCrypt.hashpw(rawPassword, BCrypt.gensalt(12))
+        BCrypt.hashpw(rawPassword + pepper, BCrypt.gensalt(12))
 
     fun verify(rawPassword: String, hashedPassword: String): Boolean =
-        BCrypt.checkpw(rawPassword, hashedPassword)
+        BCrypt.checkpw(rawPassword + pepper, hashedPassword)
 }
